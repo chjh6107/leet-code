8df1585bd0734b76b1c94524506e2dd0eaaefbe8
Jonghun Choe <54853873+chjh6107@users.noreply.github.com>
2023-10-06 20:35:38 +0900

    Time: 71 ms (26.32%), Space: 44.6 MB (61.4%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.ts
@@ -0,0 +1,13 @@
+/**
+ * Encodes a URL to a shortened URL.
+ */
+const encode = (longUrl: string): string => longUrl
+
+/**
+ * Decodes a shortened URL to its original URL.
+ */
+const decode = (shortUrl: string): string => shortUrl
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
